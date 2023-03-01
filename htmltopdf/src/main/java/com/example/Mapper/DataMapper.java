00b464d8730c4acc510bb82b1eb5fe4add5400c7
bhavani-patil211 <117839203+bhavani-patil211@users.noreply.github.com>
2023-03-01 15:01:39 +0530

    Add files via upload

---
+++ htmltopdf/src/main/java/com/example/Mapper/DataMapper.java
@@ -0,0 +1,27 @@
+package com.example.Mapper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+import com.example.model.Employee;
+
+@Service
+public class DataMapper {
+
+	public Context setData(List<Employee> empolyeeList) {
+		
+		Context context = new Context();
+		
+		Map<String, Object> data = new HashMap<>();
+		
+		data.put("employees", empolyeeList);
+		
+		context.setVariables(data);
+		
+		return context;
+	}
+}
